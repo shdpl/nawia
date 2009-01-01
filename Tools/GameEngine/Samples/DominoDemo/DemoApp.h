c63a7ff4b3208727c921f8dd8667c18dbdd6c923
volker <volker@5ce291ac-9df0-446f-9e4f-d57731c4dda7>
2009-01-01 13:49:38 +0000

    - Added domino sample based on GameEngine
    git-svn-id: http://hcm-lab.de/public/Horde3D/trunk@160 5ce291ac-9df0-446f-9e4f-d57731c4dda7

---
+++ Tools/GameEngine/Samples/DominoDemo/DemoApp.h
@@ -0,0 +1,30 @@
+#ifndef DEMOAPP_H_
+#define DEMOAPP_H_
+
+class DemoApp
+{
+public:
+
+	DemoApp();
+	~DemoApp();
+
+	bool init(const char* fileName);
+
+	bool isRunning() {return m_running;} 
+	
+	void keyHandler();
+	void mouseMoved(float x, float y);
+	void render();
+
+	static void keyboardCb(unsigned int param, bool pressed, void* userData);
+	static void mouseCb(float x, float y, void* userData);
+	static void renderCb(void* userData);
+	static void resizeCb(int width, int height);
+
+private:
+	bool m_running;
+	char m_keys[256];
+	float m_camRX, m_camRY;
+	int	  m_camID;
+};
+#endif
