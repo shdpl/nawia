9a90b861520bef1479b5a0a23a5764e99dfb2cc2
volker <volker@5ce291ac-9df0-446f-9e4f-d57731c4dda7>
2009-10-29 08:29:49 +0000

    - changed TexUnit to Sampler according to Beta4 ( TODO fix path settings and sampler names for all cases )
    git-svn-id: http://hcm-lab.de/public/Horde3D/trunk@275 5ce291ac-9df0-446f-9e4f-d57731c4dda7

--- Tools/Exporters/Blender/Horde3DExport.py
+++ Tools/Exporters/Blender/Horde3DExport.py
@@ -1285,7 +1285,7 @@ class Converter():
 			matFile.write('<Material>\n')
 			matFile.write('\t<Shader source="%s" />\n\n' % defShader)
 			for tex in self.__materials[mat]['textures']:
-				matFile.write('\t<TexUnit unit="%i" map="%s" />\n' % tex)
+				matFile.write('\t<Sampler name="albedoMap" map="%s" />\n' % tex)
 			if self.__materials[mat]['uniform'] != None:
 				color = tuple(self.__materials[mat]['uniform'])
 				name = [self.__materials[mat]['name']+'col']
